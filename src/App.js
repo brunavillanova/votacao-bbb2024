392f3ad9c32f3da7d2ef7c1d5c8ff677ae9ed5d2
bruna villanova <brunnavillanova@gmail.com>
2024-07-18 15:05:19 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,183 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import ReCAPTCHA from 'react-google-recaptcha';
+import './App.css';
+
+// Estado inicial da aplicação
+const initialState = {
+  votoEnviado: false, // Indica se o voto foi enviado
+  feedback: '', // Mensagem de feedback para o usuário
+  panorama: null, // Panorama dos votos
+  isLoading: false, // Indica se a aplicação está carregando
+  captchaToken: '', // Token do reCAPTCHA
+};
+
+// Função redutora para atualizar o estado com base nas ações
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_VOTO_ENVIADO':
+      return { ...state, votoEnviado: action.payload };
+    case 'SET_FEEDBACK':
+      return { ...state, feedback: action.payload };
+    case 'SET_PANORAMA':
+      return { ...state, panorama: action.payload };
+    case 'SET_IS_LOADING':
+      return { ...state, isLoading: action.payload };
+    case 'SET_CAPTCHA_TOKEN':
+      return { ...state, captchaToken: action.payload };
+    default:
+      return state;
+  }
+};
+
+// Componente principal da aplicação
+function App() {
+  // Usa useReducer para gerenciar o estado da aplicação
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // Função para enviar o voto
+  const enviarVoto = async (nomeCandidato) => {
+    // Verifica se o reCAPTCHA foi completado
+    if (!state.captchaToken) {
+      dispatch({ type: 'SET_FEEDBACK', payload: 'Por favor, complete o reCAPTCHA.' });
+      return;
+    }
+
+    console.log('Enviando voto para:', nomeCandidato); // Log do frontend
+    dispatch({ type: 'SET_FEEDBACK', payload: 'Enviando voto...' });
+    dispatch({ type: 'SET_IS_LOADING', payload: true });
+
+    try {
+      // Envia o voto ao backend
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/votar`, {
+        nomeCandidato,
+        captchaToken: state.captchaToken,
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      console.log('Resposta do servidor:', response); // Log da resposta do servidor
+
+      if (response.data.success) {
+        dispatch({ type: 'SET_VOTO_ENVIADO', payload: true });
+        dispatch({ type: 'SET_FEEDBACK', payload: `Voto para ${nomeCandidato} enviado com sucesso!` });
+        dispatch({ type: 'SET_PANORAMA', payload: response.data.panorama });
+      } else {
+        dispatch({ type: 'SET_FEEDBACK', payload: response.data.message || 'Erro desconhecido ao enviar o voto.' });
+      }
+    } catch (error) {
+      console.error('Erro ao enviar o voto:', error);
+      dispatch({ type: 'SET_FEEDBACK', payload: `Erro ao enviar o voto: ${error.response ? error.response.data.message : error.message}` });
+    }
+    dispatch({ type: 'SET_IS_LOADING', payload: false });
+  };
+
+  // Função para resetar o estado após o envio do voto
+  const resetarVoto = () => {
+    dispatch({ type: 'SET_VOTO_ENVIADO', payload: false });
+    dispatch({ type: 'SET_FEEDBACK', payload: '' });
+    dispatch({ type: 'SET_PANORAMA', payload: null });
+    dispatch({ type: 'SET_CAPTCHA_TOKEN', payload: '' });
+  };
+
+  // Função para lidar com mudanças no reCAPTCHA
+  const onCaptchaChange = (token) => {
+    console.log('Captcha token:', token);
+    dispatch({ type: 'SET_CAPTCHA_TOKEN', payload: token });
+    dispatch({ type: 'SET_FEEDBACK', payload: '' }); // Limpa o feedback de erro ao preencher o captcha
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <div className="container">
+          <nav className="menu-logo">
+            <div className="menu-icon">
+              <div className="menu-line"></div>
+              <div className="menu-line"></div>
+              <div className="menu-line"></div>
+            </div>
+          </nav>
+          <nav>
+            <div className="search">
+              <input type="text" placeholder="BUSCAR" aria-label="Buscar" />
+              <button aria-label="Pesquisar">
+                <svg width="10" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-search">
+                  <circle cx="11" cy="11" r="8"></circle>
+                  <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                </svg>
+              </button>
+            </div>
+            <div className="bbb-logo">
+              <p>BBB 24</p>
+            </div>
+          </nav>
+        </div>
+      </header>
+      <main>
+        <div className="container">
+          {state.votoEnviado ? (
+            <VotoEnviadoSection panorama={state.panorama} resetarVoto={resetarVoto} />
+          ) : (
+            <VotingSection
+              enviarVoto={enviarVoto}
+              isLoading={state.isLoading}
+              feedback={state.feedback}
+              onCaptchaChange={onCaptchaChange}
+            />
+          )}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+// Seção exibida após o envio do voto
+const VotoEnviadoSection = ({ panorama, resetarVoto }) => (
+  <div>
+    <h1>Obrigado por votar!</h1>
+    <p>Seu voto foi registrado com sucesso.</p>
+    {panorama && (
+      <div>
+        <h2>Panorama dos Votos</h2>
+        <p>Total de Votos: {panorama.totalVotos}</p>
+        <p>Beatriz: {panorama.percentBeatriz.toFixed(2)}%</p>
+        <p>Juliette: {panorama.percentJuliette.toFixed(2)}%</p>
+      </div>
+    )}
+    <div className="return-button">
+      <button onClick={resetarVoto}>Voltar à página inicial</button>
+    </div>
+  </div>
+);
+
+// Seção de votação
+const VotingSection = ({ enviarVoto, isLoading, feedback, onCaptchaChange }) => (
+  <div>
+    <h1>Voto Único: quem você quer que fique no BBB 24?</h1>
+    <p>Vote para definir quem deve ficar no Big Brother Brasil 2024 no primeiro Paredão da temporada!</p>
+    <div className="voting-options">
+      <p>Vote quantas vezes quiser.</p>
+      <VotingOption nome="Beatriz" enviarVoto={enviarVoto} isLoading={isLoading} />
+      <VotingOption nome="Juliette" enviarVoto={enviarVoto} isLoading={isLoading} />
+    </div>
+    <ReCAPTCHA
+      sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+      onChange={onCaptchaChange}
+    />
+    {isLoading && <p className="feedback feedback-loading">Enviando voto...</p>}
+    {feedback && !isLoading && <p className="feedback feedback-error">{feedback}</p>}
+  </div>
+);
+
+// Componente de opção de votação
+const VotingOption = ({ nome, enviarVoto, isLoading }) => (
+  <div className="option" data-name={nome.toLowerCase()}>
+    <img src={nome.toLowerCase() === 'juliette' ? '/juliete.jpg' : `${process.env.PUBLIC_URL}/${nome.toLowerCase()}.jpg`} alt={`Foto de ${nome}`} />
+    <p>{nome}</p>
+    <button onClick={() => enviarVoto(nome)} disabled={isLoading}>Votar</button>
+  </div>
+);
+
+export default App;
---
+++ src/server.js
@@ -0,0 +1,226 @@
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') }); // Carrega variáveis de ambiente do arquivo .env
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const axios = require('axios');
+const path = require('path');
+const { Schema } = mongoose;
+const morgan = require('morgan');
+const responseTime = require('response-time');
+const promClient = require('prom-client');
+
+const app = express();
+const PORT = process.env.PORT || 3003; // Usando a porta 3003
+
+console.log('RECAPTCHA_SECRET_KEY:', process.env.RECAPTCHA_SECRET_KEY);
+console.log('MONGO_URI:', process.env.MONGO_URI);
+
+// Configurar Prometheus para coletar métricas
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+collectDefaultMetrics({ timeout: 5000 });
+
+const httpRequestDurationMicroseconds = new promClient.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duração das requisições HTTP em milissegundos',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [50, 100, 200, 300, 400, 500, 750, 1000, 2000, 5000]
+});
+
+// Middleware para medir o tempo de resposta das requisições
+app.use(responseTime((req, res, time) => {
+  if (req.route) {
+    httpRequestDurationMicroseconds.labels(req.method, req.route.path, res.statusCode).observe(time);
+  }
+}));
+
+// Rota para expor métricas para o Prometheus
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', promClient.register.contentType);
+  res.end(await promClient.register.metrics());
+});
+
+// Middleware de segurança e configuração do CORS
+app.use(helmet());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware para logs e tempo de resposta
+app.use(morgan('combined')); // Logs HTTP
+app.use(responseTime((req, res, time) => {
+  console.log(`Tempo de resposta para [${req.method}] ${req.url}: ${time}ms`);
+}));
+
+// Middleware para limitar a taxa de requisições
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minuto
+  max: 1000,
+});
+app.use(limiter);
+
+// Definir o schema do MongoDB para votos
+const voteSchema = new Schema({
+  candidato: { type: String, required: true },
+  dataHora: { type: Date, default: Date.now }
+});
+const VoteModel = mongoose.model('Vote', voteSchema);
+
+// Função para calcular o panorama dos votos
+async function calcularPanorama() {
+  const totalVotos = await VoteModel.countDocuments();
+  const votosBeatriz = await VoteModel.countDocuments({ candidato: 'Beatriz' });
+  const votosJuliette = await VoteModel.countDocuments({ candidato: 'Juliette' });
+
+  const percentBeatriz = (votosBeatriz / totalVotos) * 100;
+  const percentJuliette = (votosJuliette / totalVotos) * 100;
+
+  return {
+    totalVotos,
+    percentBeatriz,
+    percentJuliette
+  };
+}
+
+// Função para calcular votos por hora
+async function calcularVotosPorHora() {
+  const votos = await VoteModel.aggregate([
+    {
+      $group: {
+        _id: {
+          ano: { $year: "$dataHora" },
+          mes: { $month: "$dataHora" },
+          dia: { $dayOfMonth: "$dataHora" },
+          hora: { $hour: "$dataHora" }
+        },
+        totalVotos: { $sum: 1 }
+      }
+    },
+    {
+      $match: { "_id.ano": { $ne: null }, "_id.mes": { $ne: null }, "_id.dia": { $ne: null }, "_id.hora": { $ne: null } }
+    },
+    {
+      $sort: { "_id": 1 }
+    }
+  ]);
+  return votos;
+}
+
+// Função para verificar o reCAPTCHA
+async function verificarRecaptcha(token) {
+  try {
+    const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify`, null, {
+      params: {
+        secret: process.env.RECAPTCHA_SECRET_KEY,
+        response: token
+      }
+    });
+    console.log('Resposta completa do reCAPTCHA:', response.data);
+    return response.data.success;
+  } catch (error) {
+    console.error('Erro na verificação do reCAPTCHA:', error);
+    return false;
+  }
+}
+
+// Rota para votar
+app.post('/api/votar', async (req, res, next) => {
+  console.log('Requisição recebida em /api/votar');
+  console.log('Corpo da requisição:', req.body);
+
+  const { nomeCandidato, captchaToken } = req.body;
+  const candidatosValidos = ['Beatriz', 'Juliette'];
+
+  if (!nomeCandidato || !candidatosValidos.includes(nomeCandidato)) {
+    console.error('Dados inválidos');
+    return res.status(400).json({ success: false, message: 'Dados de entrada inválidos. Verifique os campos e tente novamente.' });
+  }
+
+  const captchaValido = await verificarRecaptcha(captchaToken);
+  if (!captchaValido) {
+    console.error('reCAPTCHA inválido');
+    return res.status(400).json({ success: false, message: 'Falha na verificação do reCAPTCHA. Tente novamente.' });
+  }
+
+  try {
+    console.log('Salvando voto...');
+    const voto = new VoteModel({ candidato: nomeCandidato });
+    await voto.save();
+    console.log('Voto salvo com sucesso.');
+
+    const panorama = await calcularPanorama();
+
+    res.status(200).json({ success: true, message: 'Voto recebido com sucesso!', panorama });
+  } catch (error) {
+    console.error('Erro ao salvar voto:', error);
+    next(error);
+  }
+});
+
+// Rota para obter estatísticas
+app.get('/api/estatisticas', async (req, res, next) => {
+  console.log('Requisição recebida em /api/estatisticas');
+  console.log('Headers:', req.headers);
+  
+  try {
+    const totalVotos = await VoteModel.countDocuments();
+    const votosBeatriz = await VoteModel.countDocuments({ candidato: 'Beatriz' });
+    const votosJuliette = await VoteModel.countDocuments({ candidato: 'Juliette' });
+    const votosPorHora = await calcularVotosPorHora();
+
+    const panorama = {
+      totalVotos,
+      votosPorParticipante: {
+        Beatriz: votosBeatriz,
+        Juliette: votosJuliette
+      },
+      votosPorHora
+    };
+
+    res.status(200).json({ success: true, panorama });
+  } catch (error) {
+    console.error('Erro ao obter estatísticas:', error);
+    next(error);
+  }
+});
+
+// Middleware de tratamento de erros
+app.use((err, req, res, next) => {
+  console.error('Erro:', err);
+  let status = 500;
+  let message = 'Erro interno do servidor. Por favor, tente novamente mais tarde.';
+  if (err.name === 'ValidationError') {
+    status = 400;
+    message = 'Dados de entrada inválidos. Verifique os campos e tente novamente.';
+  }
+  res.status(status).json({ success: false, message });
+});
+
+// Capturar exceções não tratadas
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  // Enviar log para sistema de monitoramento ou executar ação apropriada
+});
+
+// Conexão com o MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Conexão bem-sucedida com o MongoDB.'))
+  .catch(err => console.error('Erro de conexão com o MongoDB:', err));
+
+// Servir arquivos estáticos do frontend
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor está rodando na porta ${PORT}`);
+});
