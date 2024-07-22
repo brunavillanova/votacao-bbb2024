775db0aeb37dc0e1c30c7b13e32908ec95025053
bruna villanova <brunnavillanova@gmail.com>
2024-07-22 15:11:49 -0300

    atualizando babel

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,13 @@
+// Exporta um objeto de configuração para o Babel
 module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-react']
-  };
-  
+  // Define os presets a serem usados pelo Babel
+  presets: [
+    // '@babel/preset-env' é um preset que permite usar as últimas funcionalidades do JavaScript
+    // sem se preocupar com a compatibilidade com versões antigas dos navegadores
+    '@babel/preset-env',
+    
+    // '@babel/preset-react' é um preset que permite transformar código JSX (usado em React)
+    // em código JavaScript que os navegadores podem entender
+    '@babel/preset-react'
+  ]
+};
